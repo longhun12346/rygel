f3e9e82d73a4dd407ddaea6ec7d1edd629f8122c
Niels Martignène <niels.martignene@protonmail.com>
2026-03-08 14:02:06 +0100

    Ignore version check for Debian machines

--- tools/cross/qemu.js
+++ tools/cross/qemu.js
@@ -171,11 +171,11 @@ function QemuRunner(registry = null) {
             }
 
             // Version check
-            {
+            if (machine.qemu.version != null) {
                 let filename = dirname + '/VERSION';
-                let version = fs.existsSync(filename) ? parseInt(fs.readFileSync(filename).toString(), 10) : 0;
+                let version = fs.existsSync(filename) ? fs.readFileSync(filename).toString().trim() : null;
 
-                if (version < machine.qemu.version) {
+                if (version != machine.qemu.version) {
                     self.log(machine, 'Machine version mismatch', style_ansi('[ignore]', 'gray bold'));
 
                     ignore_machines.add(machine);
